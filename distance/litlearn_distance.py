0d3ff68f889f12ea1deb337b5154efd0e099570a
Administrator <admin+iks@iks.cs.uni-magdeburg.de>
2021-02-24 14:04:25 +0000

    Rewrite prediction routine for distance computation

--- distance/litlearn_distance.py
+++ distance/litlearn_distance.py
@@ -4,7 +4,9 @@ import argparse
 import os
 from datetime import datetime
 from random import randrange
-
+from scipy import signal
+from tqdm import tqdm
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
 import obspy
@@ -14,7 +16,8 @@ import torch
 from matplotlib.dates import date2num
 from obspy import UTCDateTime
 from pytorch_lightning.loggers import TensorBoardLogger
-
+import h5py
+from sklearn.preprocessing import MinMaxScaler
 from litdatamodule_distance import LitDataModule
 from litnetwork_distance import LitNetwork
 
@@ -34,16 +37,18 @@ def learn(catalog_path, hdf5_path, model_path):
     dm = LitDataModule(catalog_path=catalog_path, hdf5_path=hdf5_path)
     logger = TensorBoardLogger("../tb_logs", name="distance")
     trainer = pl.Trainer(
-        gpus=0,
+        gpus=[0],
         logger=logger,
+        #gradient_clip_val=10,
+        track_grad_norm = 2,
     )
     trainer.fit(network, datamodule=dm)
 
     trainer.test()
 
-    now = datetime.now().strftime("%Y-%m-%d %H:%M")
-    path = "GPD_net_" + str(now) + ".pth"
-    torch.save(network.state_dict(), os.path.join(model_path, path))
+    #now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    #path = "GPD_net_" + str(now) + ".pth"
+    #torch.save(network.state_dict(), os.path.join(model_path, path))
 
 
 def test(catalog_path, waveform_path, checkpoint_path, hparams_file):
@@ -54,105 +59,81 @@ def test(catalog_path, waveform_path, checkpoint_path, hparams_file):
     )
     dm = LitDataModule(catalog_path, waveform_path)
     # init trainer with whatever options
-    trainer = pl.Trainer(gpus=1)
+    trainer = pl.Trainer(gpus=[0])
 
     # test (pass in the model)
     trainer.test(model, datamodule=dm)
 
 
-def predict(catalog_path, waveform_path, checkpoint_path):  # TODO Change to 20sec Window
+def predict(catalog_path, hdf5_path, checkpoint_path):  # TODO Change to 20sec Window
     sequence_length = 20
-    data = pd.read_csv(catalog_path)
-    print(len(data))
-    data = data[data["STATION"] != "LVC"]
-    events = sorted(data["EVENT"].unique())
-    waves = []
-    for file in os.listdir(waveform_path):
-        if file.endswith(".mseed"):
-            waves.append(file.strip(".mseed"))
-    intersect = list(set(waves).intersection(set(events)))
-    data = data[data["EVENT"].isin(intersect)]
+    split_key = "test_files"
+    file_path = hdf5_path
+    h5data = h5py.File(file_path, "r").get(split_key)
 
-    idx = randrange(0, len(data))
-    event, station, p_pick, time = data.iloc[idx][
-        ["EVENT", "STATION", "P_PICK", "TIME"]
-    ]  # TODO time means start time of event?
-    waveform = obspy.read(os.path.join(waveform_path, f"{event}.mseed"))
-    station_stream = waveform.select(station=station, channel="HH*")  # high frequency
-    while not station_stream:
-        idx = randrange(0, len(data))
-        event, station, p_pick, time = data.iloc[idx][
-            ["EVENT", "STATION", "P_PICK", "TIME"]
-        ]  # TODO time means start time of event?
-        waveform = obspy.read(os.path.join(waveform_path, f"{event}.mseed"))
-        station_stream = waveform.select(station=station, channel="HH*")  # high frequency
+    catalog = pd.read_csv(catalog_path)
+    train = catalog[catalog["SPLIT"] == "TRAIN"]
+    dist = np.append(np.array(train["DIST"]), [1, 600000])
+    
+    scaler = MinMaxScaler()
+    scaler.fit(dist.reshape(-1, 1))
+    #train_scaled = scaler.transform(train.reshape(-1, 1))
+    
+    print(scaler.data_max_)
 
-    station_stream.plot()
-    # to prevent loading the entire catalog, choose a random event and check, whether it is in the waveform folder
-
-    # print now maybe with highlight on the P Pick
-    # maybe slice out with beginning on start time?
     model = LitNetwork.load_from_checkpoint(
         checkpoint_path=checkpoint_path,
 
     )
     model.freeze()
 
-    out_times = np.arange(
-        station_stream[0].stats.starttime, station_stream[0].stats.endtime + 1, 1
-    )
-    outs = np.zeros(len(out_times))
-    for i in range(0, len(out_times) - 4):
-        station_stream = waveform.select(station=station, channel="HH*")  # reload
-        window = station_stream.slice(starttime=out_times[i], endtime=out_times[i] + 4)
-        window.filter("highpass", freq=2, zerophase=True)
-        window.detrend().normalize()
+    test = catalog[catalog["SPLIT"] == "TEST"]
+    idx = randrange(0, len(test))
+    event, station, distance = test.iloc[idx][["EVENT", "STATION", 'DIST']]
+    print(distance)
+    print(scaler.transform(distance.reshape(1, -1)))
+    
+    waveform = np.array(h5data.get(event + "/" + station))
+    filt = signal.butter(
+            2, 2, btype="highpass", fs=100, output="sos"
+        )
+    
+    real_output = np.zeros(6000-2000)
+    real_labels = np.zeros(6000-2000)
+    s_output= np.zeros(6000-2000)
+    s_labels= np.zeros(6000-2000)
+    for i in tqdm(range(0, 6000 - 20*100)):
+        
+        
+        station_stream = waveform[:, i: i+20*100]
+        station_stream = signal.sosfilt(filt, station_stream, axis=-1).astype(
+            np.float32
+        )
+        station_stream = signal.detrend(station_stream)
+        station_stream = signal.normalize(station_stream, 1)[0]
+        station_stream = torch.from_numpy(station_stream[None])
 
-        # for windowed_st in station_stream.slide(window_length=sequence_length, step=1):
-        #     window = windowed_st.copy()
-        #     window.detrend().normalize()
-        trace_z = np.float32(window[0])
-        trace_n = np.float32(window[1])
-        trace_e = np.float32(window[2])
-        example = np.stack((trace_z, trace_n, trace_e))[:, 0:-1]
-        example = torch.from_numpy(example[None])
-        output = model(example)
-        _, predicted = torch.max(output, 1)
-
-        outs[i: i + 4] = outs[i: i + 4] + np.full(4, predicted[0])
-        # print(outs)
-    #    print(windowed_st)
-
-    #    print("---")
-
-    print(outs)
-    out_times = date2num(out_times)
-
-    tr1 = station_stream[0]
-    tr2 = station_stream[1]
-    tr3 = station_stream[2]
-
-    fig, ax = plt.subplots(4, sharex=True)
-    ax[0].plot(tr1.times("matplotlib"), tr1.data, color="black", linewidth=0.4)
-    ax[0].xaxis_date()
-    ax[0].axvline(date2num(UTCDateTime(p_pick)), color="blue")
-
-    ax[1].plot(tr2.times("matplotlib"), tr2.data, color="black", linewidth=0.4)
-    ax[1].xaxis_date()
-    ax[1].axvline(date2num(UTCDateTime(p_pick)), color="blue")
-
-    ax[2].plot(tr3.times("matplotlib"), tr3.data, color="black", linewidth=0.4)
-    ax[2].xaxis_date()
-    ax[2].axvline(date2num(UTCDateTime(p_pick)), color="blue")
-
-    ax[3].plot(out_times, outs, "r-")
-    ax[3].xaxis_date()
-    fig.autofmt_xdate()
-    plt.show()
+        out = model(station_stream).squeeze()
+        out = np.array(out)
+        s_output[i] = out
+        s_labels[i] = scaler.transform(distance.reshape(1,-1))
+        real_output[i] = scaler.inverse_transform(out.reshape(1,-1))
+        real_labels[i] = distance
+                                             
+    print(real_output)
+    print(real_labels)
+    print(s_output)
+    print(s_labels)
+    print(mean_squared_error(real_output,real_labels))
+    print(mean_squared_error(s_output,s_labels))
+    
+    plt.plot(real_output,real_labels,'r')
     plt.savefig("current_plot")
+    plt.plot(s_output,s_labels,'b')
+    plt.savefig("cr2")
 
 
-learn(cp, hp, mp)
+#learn(cp, hp, mp)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--action', type=str, required=True)
@@ -176,8 +157,8 @@ if __name__ == '__main__':
              hparams_file=args.hparams_file,
              )
     if action == 'predict':
-        test(catalog_path=args.catalog_path,
-             waveform_path=args.waveform_path,
+        predict(catalog_path=args.catalog_path,
+             hdf5_path = args.hdf5_path,
              checkpoint_path=args.checkpoint_path,
-             hparams_file=args.hparams_file,
+             
              )
