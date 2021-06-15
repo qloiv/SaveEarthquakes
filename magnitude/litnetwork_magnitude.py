c942b9c8abf75a414644f19b4d80f54c364dc621
maeyoqeia <w.wassertal@gmx.de>
2021-06-15 15:45:03 +0200

    Build magnitude network (from distance network) with MSELoss, add normalization factor

--- magnitude/litnetwork_magnitude.py
+++ magnitude/litnetwork_magnitude.py
@@ -1,8 +1,7 @@
-import pytorch_lightning as pl
-import torch
+import torch.nn
 from pytorch_lightning.core.lightning import LightningModule
-from torch.nn import CrossEntropyLoss
-from torch.nn import Linear, ReLU, Flatten, Sequential, Conv1d, MaxPool1d, BatchNorm1d
+from torch.nn import Linear, ReLU, Flatten, Sequential, Conv1d, MaxPool1d, BatchNorm1d, Softplus, \
+    MSELoss  # GaussianNLLLoss
 
 
 class LitNetwork(LightningModule):
@@ -12,98 +11,100 @@ class LitNetwork(LightningModule):
             Conv1d(3, 32, kernel_size=21, stride=1, padding=10),
             # pytorch hat noch keine padding_mode = same implementation
             BatchNorm1d(
-                32, momentum=0.99, eps=0.001
+                32
             ),  # batch norm values taken from keras default
             ReLU(),
             MaxPool1d(kernel_size=2, stride=2),
         )
         self.cnn_layer2 = Sequential(
             Conv1d(32, 64, kernel_size=15, stride=1, padding=7),
-            BatchNorm1d(64, momentum=0.99, eps=0.001),
+            BatchNorm1d(64),
             ReLU(),
             MaxPool1d(kernel_size=2, stride=2),
         )
         self.cnn_layer3 = Sequential(
             Conv1d(64, 128, kernel_size=11, stride=1, padding=5),
-            BatchNorm1d(128, momentum=0.99, eps=0.001),
+            BatchNorm1d(128),
             ReLU(),
             MaxPool1d(kernel_size=2, stride=2),
         )
         self.cnn_layer4 = Sequential(
             Conv1d(128, 256, kernel_size=9, stride=1, padding=4),
-            BatchNorm1d(256, momentum=0.99, eps=0.001),
+            BatchNorm1d(256),
             ReLU(),
             MaxPool1d(kernel_size=2, stride=2),
-            Flatten()
+        )
+        self.cnn_layer5 = Sequential(
+            Conv1d(256, 256, kernel_size=7, stride=1, padding=3),
+            BatchNorm1d(256),
+            ReLU(),
+            MaxPool1d(kernel_size=2, stride=2),
+        )
+        self.cnn_layer6 = Sequential(
+            Conv1d(256, 256, kernel_size=5, stride=1, padding=2),
+            BatchNorm1d(256),
+            ReLU(),
+            MaxPool1d(kernel_size=2, stride=2),
         )
 
-        self.linear_layers = Sequential(
-            Linear(25 * 256 * 5, 200),  # the keras network uses 200 units, so...
-            BatchNorm1d(200, momentum=0.99, eps=0.001),
-            ReLU(),
-            Linear(200, 9),
-            BatchNorm1d(9, momentum=0.99, eps=0.001),
+        self.flatten_layer = Flatten()
+
+        self.linear_layers1 = Sequential(
+            Linear(256 * 31 + 1, 200),  # the keras network uses 200 units, so...
+            # BatchNorm1d(200),
             ReLU(),
         )
-
-        self.test_acc = pl.metrics.Accuracy()
-        self.train_acc = pl.metrics.Accuracy()
-        self.val_acc = pl.metrics.Accuracy()
+        self.linear_layers2 = Sequential(
+            Linear(200, 1),
+            Softplus(),
+        )
 
     def forward(self, x):
+        max_str = x[1]
+        x = x[0]
         x = self.cnn_layer1(x)
         x = self.cnn_layer2(x)
         x = self.cnn_layer3(x)
         x = self.cnn_layer4(x)
-        x = self.linear_layers(x)
+        x = self.cnn_layer5(x)
+        x = self.cnn_layer6(x)
+
+        x = self.flatten_layer(x)
+        x = torch.cat((x, max_str.view(x.shape[0], 1)), dim=1)
+        x = self.linear_layers1(x)
+        x = self.linear_layers2(x)
         return x
 
     def training_step(self, inputs, inputs_idx):
         waveform = inputs["waveform"]
         label = inputs["label"]
-        outputs = self(waveform)
-        criterion = CrossEntropyLoss()
-        loss = criterion(outputs, label)
-        _, predicted = torch.max(outputs.data, 1)
-        self.train_acc(predicted, label)
-        self.log("train_acc", self.train_acc)
+        outputs = self(waveform).squeeze()
+        x = outputs
+        criterion = MSELoss()
+        loss = criterion(x, label)
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, inputs, inputs_idx):
         waveform = inputs["waveform"]
         label = inputs["label"]
-        outputs = self(waveform)
-        criterion = CrossEntropyLoss()
-        loss = criterion(outputs, label)
-        _, predicted = torch.max(outputs.data, 1)
-        self.val_acc(predicted, label)
+        outputs = self(waveform).squeeze()
+        x = outputs
+        criterion = MSELoss()
+        loss = criterion(x, label)
         self.log("val_loss", loss)
-        self.log("val_acc", self.val_acc)
         return loss
 
     def test_step(self, inputs, inputs_idx):
         waveform = inputs["waveform"]
         label = inputs["label"]
-        outputs = self(waveform)
-        criterion = CrossEntropyLoss()
-        loss = criterion(outputs, label)
-        _, predicted = torch.max(outputs.data, 1)
-        self.test_acc(predicted, label)
-        self.log("test_acc", self.test_acc)
+        outputs = self(waveform).squeeze()
+        x = outputs
+        criterion = MSELoss()
+        loss = criterion(x, label)
         self.log("test_loss", loss)
-        return {'loss': loss, 'preds': predicted, 'target': label}
-
-    # def test_epoch_end(self, outputs):
-    #    preds = torch.cat([tmp['preds'] for tmp in outputs])
-    #    targets = torch.cat([tmp['target'] for tmp in outputs])
-    #    confusion_matrix = pl.metrics.functional.confusion_matrix(preds, targets, num_classes=7)
-
-    #   df_cm = pd.DataFrame(confusion_matrix.cpu().numpy(), index = range(1,8), columns=range(1,8))
-    #   plt.figure(figsize = (10,8))
-    #   fig_ = sns.heatmap(df_cm, annot=True, cmap='Spectral').get_figure()
-    #   fig_.savefig("confusion matrix")
+        return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters())
+        optimizer = torch.optim.Adam(self.parameters())  # Adam(self.model.parameters(), lr=0.001) is default lr
         return optimizer
