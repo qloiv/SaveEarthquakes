32772f377edf0962a205bf9f969e183df9b7bdb7
Administrator <admin+iks@iks.cs.uni-magdeburg.de>
2021-07-12 08:53:03 +0000

    Check for empty files

--- dataset_formula.py
+++ dataset_formula.py
@@ -5,7 +5,7 @@ import numpy as np
 import obspy
 import pandas as pd
 from obspy import UTCDateTime
-
+from tqdm import tqdm
 
 def obspy_detrend(data):
     # based on obspys detrend ("simple") function
@@ -34,79 +34,78 @@ def normalize_stream(stream, global_max=False):
     return stream, stream_max
 
 
-cp = "/home/viola/WS2021/Code/Daten/Chile_small/new_catalog_sensitivity.csv"
-wp = "/home/viola/WS2021/Code/Daten/Chile_small/mseedJan07/"
-hp = "/home/viola/WS2021/Code/Daten/Chile_small/hdf5_dataset_sensitivity.h5"
-mp = "/home/viola/WS2021/Code/Models"
-chp = "/home/viola/WS2021/Code/tb_logs/distance/version_47/checkpoints/epoch=19-step=319.ckpt"
-hf = "/home/viola/WS2021/Code/tb_logs/distance/version_47/hparams.yaml",
-ip = "/home/viola/WS2021/Code/Daten/Chile_small/inventory.xml"
+#cp = "/home/viola/WS2021/Code/Daten/Chile_small/new_catalog_sensitivity.csv"
+#wp = "/home/viola/WS2021/Code/Daten/Chile_small/mseedJan07/"
+#hp = "/home/viola/WS2021/Code/Daten/Chile_small/hdf5_dataset_sensitivity.h5"
+#mp = "/home/viola/WS2021/Code/Models"
+#chp = "/home/viola/WS2021/Code/tb_logs/distance/version_47/checkpoints/epoch=19-step=319.ckpt"
+#hf = "/home/viola/WS2021/Code/tb_logs/distance/version_47/hparams.yaml",
+#ip = "/home/viola/WS2021/Code/Daten/Chile_small/inventory.xml"
+cp = "../new_catalogue_sensitivity.csv"
+wp= "../../data/earthquake/waveforms_long_full"
+wp2 = "../../data/earthquake/waveforms_long_additional"
+ip = "../inventory.xml"
 
-
-def formulate(x):
+def formulate(x, train_catalog):
     # load train catalog
     catalog_path = cp
-    hdf5_path = hp
+    #hdf5_path = hp
     waveform_path = wp
-    # waveform_path_add=
+    waveform_path_add=wp2
     inv_path = ip
-    catalog = pd.read_csv(catalog_path)
-    train_catalog = catalog[catalog["SPLIT"] == "TRAIN"]
+
     #    split_key = "train_files"
     #  file_path = hdf5_path
     #  h5data = h5py.File(file_path, "r").get(split_key)
-
-    # load magnitude
-    magnitude = np.array(train_catalog["MA"])
-    # load distance
-    distance = np.array(train_catalog["DIST"])
 
     # for every item in catalog
     # locate 4 seconds after p Pick --> this is clear as its 3000-3003
     # compute peak displacement
     # load catalog with random test event
 
-    max_disp = np.zeros(100)
-    for idx in range(x, x + 100):
-        event, station, p_pick = train_catalog.iloc[idx][["EVENT", "STATION", "P_PICK"]]
-        print(idx)
+    event, station, p_pick,distance, magnitude = train_catalog[["EVENT", "STATION", "P_PICK", "DIST","MA"]]
+        #print(idx)
         # load hdf5 waveform
         # raw_waveform = np.array(h5data.get(event + "/" + station))
         #  seq_len = 4 * 100  # 4seconds *sampling rate
         # p_pick_array = 3000  # ist bei 3000 weil obspy null indiziert arbeitet, also die Startzeit beginnt bei array 0
 
         # load obpsy waveforms
-        o_raw_waveform = (obspy.read(os.path.join(waveform_path, f"{event}.mseed")))  # + (
-        # obspy.read(os.path.join(waveform_path_add, f"{event}.mseed"))
-        # )
+    if os.path.getsize(os.path.join(waveform_path_add, f"{event}.mseed")) > 0:
+            o_raw_waveform = obspy.read(os.path.join(waveform_path, f"{event}.mseed")) + obspy.read(os.path.join(waveform_path_add, f"{event}.mseed"))
+    else:
+            o_raw_waveform = obspy.read(os.path.join(waveform_path, f"{event}.mseed"))
 
-        o_waveform = o_raw_waveform.select(station=station, channel="HHZ")
-        o_station_stream = o_waveform.slice(
+
+
+    o_waveform = o_raw_waveform.select(station=station, channel="HHZ")
+    o_station_stream = o_waveform.slice(
             starttime=UTCDateTime(p_pick),  #
             endtime=UTCDateTime(p_pick) + 3.99,
-        )  # -0.01 deletes the last item, therefore enforcing array indexing
+    )  # -0.01 deletes the last item, therefore enforcing array indexing
 
         # load inventory
-        inv = obspy.read_inventory(inv_path)
-        inv_selection = inv.select(station=station, channel="HHZ")
+    inv = obspy.read_inventory(inv_path)
+    inv_selection = inv.select(station=station, channel="HHZ")
 
-        new_stream_w30 = o_station_stream.copy()
+    new_stream_w30 = o_station_stream.copy()
 
-        disp_w30 = new_stream_w30.copy().remove_response(
-            inventory=inv_selection, pre_filt=None, output="DISP", water_level=60
-        )
+    disp_w30 = new_stream_w30.remove_response(
+        inventory=inv_selection, pre_filt=None, output="DISP", water_level=30
+    )
         # disp_w30.plot()
-        m = disp_w30.max()  # already returns absolute maximum amplitude
-        max_disp[idx] = m[0]
-    print("done")
-
-    max_disp = max_disp / 100
-    print(max_disp)
-    # np.save("max_disp.npy", max_disp)
-    return max_disp
+    m = disp_w30.max()[0]  # already returns absolute maximum amplitude
+    #print(m*100)
+    return (m*100,distance,magnitude)
 
 
-pool = Pool(4)
-len_train = 2045
-results = [pool.apply(formulate, args=(x,)) for x in np.arange(0, len_train)]
-print(results)
+
+pool = Pool(35)
+catalog_path = "../new_catalogue_sensitivity.csv"
+catalog = pd.read_csv(catalog_path)
+train_catalog = catalog[catalog["SPLIT"] == "TRAIN"]
+len_train = len(train_catalog)
+results = [pool.apply_async(formulate, args=(x,train_catalog.iloc[x])) for x in tqdm(np.arange(0, len_train))]
+output = [p.get() for p in tqdm(results)]
+np.save("/project/SaveEarthquakes/output.npy",output)
+print(output)
