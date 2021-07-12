b7dae6289303399964c59c10a0be5a6713e9749f
Administrator <admin+iks@iks.cs.uni-magdeburg.de>
2021-07-12 15:00:02 +0000

    Add logarithm and fix absolute values

--- dataset_regression.py
+++ dataset_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 data = np.load("output.npy")
 print(data.shape)
@@ -7,9 +8,15 @@ pd = data[:, 0]
 dist = data[:, 1]
 mag = data[:, 2]
 print(pd, dist, mag)
+data[:,0] = np.abs(data[:,0]) # is the maximum is already the absolute max, we just have to also pass the absolute value
 data[:, 1] = data[:, 1] / 1000  # scale from meter to km
-X = data[:, 0:2]  # X data is peak displacement and distance
+X = np.log(data[:, 0:2])  # X data is peak displacement and distance
+print(X, X.shape)
 y = data[:, 2]  # the magnitude
+print(y,y.shape)
 reg = LinearRegression().fit(X, y)
 coefs = reg.coef_
-print(coefs)
+print(coefs[0])
+print(coefs[1])
+plt.scatter(X[:,0],y)
+plt.savefig("regression.png")
